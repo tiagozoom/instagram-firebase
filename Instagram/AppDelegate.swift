93ac615a9e26df2e6297ab6a67dd3061cf4e8808
tiago henrique <tiagohenrique@tiagos-Mac-mini.local>
2018-04-04 11:26:35 -0300

    Removing local persistence

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -15,11 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        Database.database().isPersistenceEnabled = true
         window = UIWindow(frame: UIScreen.main.bounds)
         let rootViewController = MainTabController()
         window?.rootViewController = rootViewController
